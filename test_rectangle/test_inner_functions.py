67b8e91adb4f088bd02ed513196a01a76978085f
Omar <gomar@dmacc.edu>
2019-10-02 21:03:21 -0700

    Initial commit

---
+++ test_rectangle/test_inner_functions.py
@@ -0,0 +1,12 @@
+import unittest
+
+from rectangle import inner_functions_assignment
+
+
+class MyTestCase(unittest.TestCase):
+    def test_measurements_area(self):
+        self.assertEqual(inner_functions_assignment.measurements([4, 3.4]), 'Area  = 13.6','Perimeter  = 14.8')
+
+
+if __name__ == '__main__':
+    unittest.main()
